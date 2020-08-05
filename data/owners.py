e6e68fd8b93a80b3ea9fcb72dd922a923714546e
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-05 20:56:36 +0530

    updated owners file

---
+++ data/owners.py
@@ -0,0 +1,15 @@
+import datetime
+import mongoengine
+
+class Owners(mongoengine.Document):
+    registered_date = mongoengine.DateTimeField(default= datetime.datetime.now)
+    name = mongoengine.StringField(required = True)
+    email = mongoengine.StringField(required = True)
+
+    snake_ids = mongoengine.ListField()
+    cage_ids = mongoengine.ListField()
+
+    meta ={
+        'db_alias' : 'core' ,
+        'collection' : 'owners'
+    }
