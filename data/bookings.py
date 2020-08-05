4ff660d84744f7fa3f9233ab5357d1d986676745
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-08-05 20:52:53 +0530

    updated bookings file

---
+++ data/bookings.py
@@ -0,0 +1,12 @@
+import mongoengine
+
+class Booking(mongoengine.EmbeddedDocument):
+    guest_owner_id = mongoengine.ObjectIdField()
+    guest_snake_id = mongoengine.ObjectIdField()
+
+    booked_date = mongoengine.DateTimeField()
+    check_in_date = mongoengine.DateTimeField(required = True)
+    check_out_date = mongoengine.DateTimeField(required = True)
+
+    review = mongoengine.StringField()
+    rating = mongoengine.IntField(default = 0)
